d92015ca9a786d1577b31be963ba0d7bb5022ba8
realsubodh <subodhsingh3477@gmail.com>
2025-10-07 22:31:43 +0530

    adding mongodb

---
+++ backend/src/config/db.ts
@@ -0,0 +1,11 @@
+// import { model, Schema } from "mongoose";
+
+// const UserSchema = new Schema({
+//     username : {type: String, unique:true},
+//     password : String
+// })
+
+// export const UserModel = model("User", UserSchema)
+
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGO_URL)
