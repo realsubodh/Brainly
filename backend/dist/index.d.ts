2868522c7dd86040382e8c3fc9022b7abead6e32
realsubodh <subodhsingh3477@gmail.com>
2026-02-07 21:55:38 +0530

    inspection found that whole backend is corrupted so going to start from scratch

--- backend/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import "dotenv/config";
-import "./config/db.js";
-//# sourceMappingURL=index.d.ts.map
--- backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import "dotenv/config";  
-
-import "./config/db.js"
-
-const app = express();
-app.use(express.json());
-
-app.listen(3000, ()=>console.log("App is running on Port 3k✅"))
